765033c1d017e0af22fe6827ee35403bf1feda14
GallyP <Prikhgalya@gmail.com>
2020-11-09 14:29:29 +0300

    first commit

---
+++ HelloWorld/Class2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+//Главный класс программы
+    class HelloWorld1
+    {
+        //Главный метод программы
+        static void Main()
+        {
+            //Отображение сообщения в консольном окне:
+            Console.WriteLine("Изучаем git000");
+        }
+    }
